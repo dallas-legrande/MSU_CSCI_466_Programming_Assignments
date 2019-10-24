0162ac62caf104083393bc77c1d7456dd7c7b2b7
dallas-legrande <dallas.legrande@gmail.com>
2019-10-23 19:48:37 -0600

    Had to change the packet splitting to account for the address being tacked on to the data. so that the total packet size was under the mtu.

--- network.py
+++ network.py
@@ -5,7 +5,6 @@ Created on Oct 12, 2016
 '''
 import queue
 import threading
-import re
 
 
 ## wrapper class for a queue of packets
@@ -34,7 +33,7 @@ class Interface:
 # NOTE: This class will need to be extended to for the packet to include
 # the fields necessary for the completion of this assignment.
 class NetworkPacket:
-    ## packet encoding lengths. 5 character address length
+    ## packet encoding lengths 
     dst_addr_S_length = 5
     
     ##@param dst_addr: address of the destination host
@@ -58,9 +57,9 @@ class NetworkPacket:
     @classmethod
     def from_byte_S(self, byte_S):
         dst_addr = int(byte_S[0 : NetworkPacket.dst_addr_S_length])
-        data_S = byte_S[NetworkPacket.dst_addr_S_length: ]
+        data_S = byte_S[NetworkPacket.dst_addr_S_length : ]
         return self(dst_addr, data_S)
-
+    
 
     
 
@@ -77,22 +76,14 @@ class Host:
     ## called when printing the object
     def __str__(self):
         return 'Host_%s' % (self.addr)
-
-    #split the data being sent into smaller packets size of the mtu
-    def split_data(self, data_S, mtu_S):
-        sm_p = [(data_S[i:i + mtu_S]) for i in range(0, len(data_S), mtu_S)]
-        return sm_p
        
     ## create a packet and enqueue for transmission
     # @param dst_addr: destination address for the packet
     # @param data_S: data being transmitted to the network layer
-    #break large packets up into smaller packets
     def udt_send(self, dst_addr, data_S):
-        sm_p = self.split_data(data_S, self.out_intf_L[0].mtu - NetworkPacket.dst_addr_S_length)
-        for i in sm_p:
-            p = NetworkPacket(dst_addr, i)
-            self.out_intf_L[0].put(p.to_byte_S())  # send packets always enqueued successfully
-            print('%s: sending packet "%s" on the out interface with mtu=%d' % (self, p, self.out_intf_L[0].mtu))
+        p = NetworkPacket(dst_addr, data_S)
+        self.out_intf_L[0].put(p.to_byte_S()) #send packets always enqueued successfully
+        print('%s: sending packet "%s" on the out interface with mtu=%d' % (self, p, self.out_intf_L[0].mtu))
         
     ## receive packet from the network layer
     def udt_receive(self):
@@ -102,13 +93,13 @@ class Host:
        
     ## thread target for the host to keep receiving data
     def run(self):
-        print(threading.currentThread().getName() + ': Starting')
+        print (threading.currentThread().getName() + ': Starting')
         while True:
             #receive data arriving to the in interface
             self.udt_receive()
             #terminate
             if(self.stop):
-                print(threading.currentThread().getName() + ': Ending')
+                print (threading.currentThread().getName() + ': Ending')
                 return
         
 
@@ -153,9 +144,9 @@ class Router:
                 
     ## thread target for the host to keep forwarding data
     def run(self):
-        print(threading.currentThread().getName() + ': Starting')
+        print (threading.currentThread().getName() + ': Starting')
         while True:
             self.forward()
             if self.stop:
-                print(threading.currentThread().getName() + ': Ending')
-                return
+                print (threading.currentThread().getName() + ': Ending')
+                return 
